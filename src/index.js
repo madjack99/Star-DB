0bb8040b971c227496887dea7b191f1565313606
madjack <kavanokami@gmail.com>
2020-01-27 14:52:49 +0400

    feat: create basic layout for components

--- src/index.js
+++ src/index.js
@@ -1,45 +1,6 @@
-class SwapiService {
-  _apiBase = 'https://swapi.co/api';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-  async getResource(url) {
-    const res = await fetch(`${this._apiBase}${url}`);
+import App from './components/app';
 
-    if (!res.ok) {
-      throw new Error(`Could not fetch ${url} 
-      , received ${res.status}`);
-    }
-
-    return await res.json();
-  }
-
-  async getAllPeople() {
-    const res = await this.getResource(`/people/`);
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResource(`/people/${id}`);
-  }
-
-  async getAllPlanets() {
-    const res = await this.getResource(`/planets/`);
-    return res.results;
-  }
-
-  getPlanet(id) {
-    return this.getResource(`/planet/${id}`);
-  }
-
-  async getAllStarships() {
-    const res = await this.getResource(`/starships/`);
-    return res.results;
-  }
-
-  getStarship(id) {
-    return this.getResource(`/starship/${id}`);
-  }
-}
-
-const swapi = new SwapiService();
-
-swapi.getPerson(3).then(person => console.log(person.name));
+ReactDOM.render(<App />, document.querySelector('#root'));
