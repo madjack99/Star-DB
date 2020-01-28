9c806d9f3f38e1524cccb7e429d81b932477cb25
madjack <kavanokami@gmail.com>
2020-01-28 19:05:42 +0400

    feat: user item-list to display diff collections

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,15 +4,46 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import PeoplePage from '../people-page';
 
+import PersonDetails from '../person-details';
+import ItemList from '../item-list';
+
+import SwapiService from '../../services/swapi-service';
+
 import './app.css';
 
 export default class App extends Component {
+  swapiService = new SwapiService();
+
   render() {
     return (
       <div>
         <Header />
         <RandomPlanet />
         <PeoplePage />
+        <br />
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}
+            />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails />
+          </div>
+        </div>
+        <br />
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships}
+            />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails />
+          </div>
+        </div>
       </div>
     );
   }
