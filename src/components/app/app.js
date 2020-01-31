c2c98d5808f83e67632d3bf67b8a5bbd8a48857a
madjack <kavanokami@gmail.com>
2020-01-31 15:10:12 +0400

    refactor: app, create person/planet/starship-pages

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,19 +2,7 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import PeoplePage from '../people-page';
-import ItemList from '../item-list';
-import {
-  PersonDetails,
-  PlanetDetails,
-  StarshipDetails,
-  PersonList,
-  PlanetList,
-  StarshipList,
-} from '../sw-components';
-
-import Row from '../row';
-import ItemDetails, { Record } from '../item-details';
+import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
 
 import SwapiService from '../../services/swapi-service';
 import { SwapiServiceProvider } from '../swapi-service-context';
@@ -25,31 +13,15 @@ export default class App extends Component {
   swapiService = new SwapiService();
 
   render() {
-    const {
-      getPerson,
-      getStarship,
-      getPersonImage,
-      getStarshipImage,
-      getAllPeople,
-      getAllPlanets,
-    } = this.swapiService;
-
     return (
       <div>
         <SwapiServiceProvider value={this.swapiService}>
           <Header />
           <RandomPlanet />
 
-          <PersonDetails itemId={11} />
-          <PlanetDetails itemId={5} />
-          <StarshipDetails itemId={9} />
-          {/* <PeoplePage /> */}
-
-          {/* <Row left={personDetails} right={starshipDetails} /> */}
-
-          <PersonList />
-          <StarshipList />
-          <PlanetList />
+          <PeoplePage />
+          <PlanetsPage />
+          <StarshipsPage />
         </SwapiServiceProvider>
       </div>
     );
--- src/components/sw-components/item-lists.js
+++ src/components/sw-components/item-lists.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ItemList from '../item-list';
 import withData from '../hoc-helper/with-data';
 import { withSwapiService } from '../hoc-helper';
-import SwapiService from '../../services/swapi-service';
-
-const swapiService = new SwapiService();
-
-const { getAllPeople, getAllPlanets, getAllStarships } = swapiService;
 
 const withChildFunction = (WrappedComponent, fn) => {
   return props => {
