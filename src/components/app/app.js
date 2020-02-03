41312f41f44ad15bbbf6c0751785ca078eed6011
madjack <kavanokami@gmail.com>
2020-02-03 14:54:05 +0400

    feat: add dynamic path

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
 import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages';
+import StarshipDetails from '../sw-components/starship-details';
 
 import SwapiService from '../../services/swapi-service';
 import { SwapiServiceProvider } from '../swapi-service-context';
@@ -24,7 +25,14 @@ export default class App extends Component {
             <Route path="/" render={() => <h2>Welcome to star DB</h2>} exact />
             <Route path="/people" component={PeoplePage} />
             <Route path="/planets" component={PlanetsPage} />
-            <Route path="/starships" component={StarshipsPage} />
+            <Route path="/starships" exact component={StarshipsPage} />
+            <Route
+              path="/starships/:id"
+              render={({ match }) => {
+                const { id } = match.params;
+                return <StarshipDetails itemId={id} />;
+              }}
+            />
           </Router>
         </SwapiServiceProvider>
       </div>
